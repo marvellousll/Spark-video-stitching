17bd0ececbbf37e0263fd536f34f16b8b715cae0
lliu0809 <44155608+lliu0809@users.noreply.github.com>
2021-03-15 22:19:25 -0700

    updated video2frame

--- v2f.py
+++ v2f.py
@@ -1,6 +1,7 @@
-# To run: python3 v2f.py arg1 arg2
+# To run: python3 v2f.py arg1 arg2 arg3
 # arg1: path of the source video 
 # arg2: path for the frames to be stored
+# arg3: name of video
 
 import cv2
 import os
@@ -11,6 +12,7 @@ import sys
 # /Users/hangl/Desktop/CS219/CS219-W21-Spark/frames
 video = cv2.VideoCapture(sys.argv[1])
 framePath = sys.argv[2]
+name = sys.argv[3]
 
 sec = 0
 frameRate = 0.05 # 20 fps
@@ -20,7 +22,11 @@ def videoToFrames(sec):
     video.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
     success,image = video.read()
     if success:
-       	cv2.imwrite(os.path.join(framePath, "image"+str(count)+".jpg"), image)
+    	directory = framePath + '/' + 'frame' + str(count)
+    	if not os.path.exists(directory):
+        	os.makedirs(directory)
+    	cv2.imwrite(os.path.join(directory, name+".jpg"), image)
+
     return success
 
 
